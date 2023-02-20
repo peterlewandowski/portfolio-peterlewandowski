5a7574db88cbef20c0564bc3d7b34fd2cf153d23
Peter Lewandowski <mapiotr2010@gmail.com>
2023-02-19 21:13:05 -0500

    added Hero boilerplate, refactored HomePage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,9 @@
-// import links from '../components/common/HeaderLinks.json';
-// import data from '../components/common/FooterLinks.json';
-import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-// import { HeaderMiddle as Header } from '../components/common/Header';
-// import { FooterLinks as Footer } from '../components/common/Footer';
+import { HeroContentLeft as Hero } from '../components/Hero';
 
 export default function HomePage() {
   return (
     <>
-      <Welcome />
-      <ColorSchemeToggle />
+      <Hero />
     </>
   );
 }
