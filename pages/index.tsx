cae8ca227339ebbc03b376587254706d12f1f630
Peter Lewandowski <mapiotr2010@gmail.com>
2023-02-20 09:17:52 -0500

    added FeaturedStack and Projects sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
+import featuredStackData from '../components/home/FeaturedStackdata.json';
+import ProjectsData from '../components/home/ProjectsData.json';
 import { ContactIcons as ContactUs } from '../components/home/ContactUs';
+import { FeaturesImages as FeaturedStack } from '../components/home/FeaturedStack';
 import { HeroContentLeft as Hero } from '../components/home/Hero';
+import { BadgeCard as Projects } from '../components/home/Projects';
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+      <FeaturedStack {...featuredStackData} />
+      <Projects {...ProjectsData} />
       <ContactUs />
     </>
   );
